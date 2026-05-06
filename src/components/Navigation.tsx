aa4452cdc9e5392b590f3dac54fd03beb8403b23
ibrahimh-max <ih8445807@gmail.com>
2026-05-06 06:45:23 +0530

    pages incoming

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -9,7 +9,7 @@ export default function Navigation() {
         <div className="flex justify-between items-center h-16">
           <div className="flex items-center space-x-8">
             <Link href="/" className="text-xl font-bold text-blue-600">
-              HospitalityHire
+              SHIRE
             </Link>
             
             <div className="hidden md:flex space-x-6">
