e038d73f936a50f66ce0934846b7978f8584cecb
ibrahimh-max <ih8445807@gmail.com>
2026-06-13 04:10:46 +0530

    lets see

---
+++ src/app/app/layout.tsx
@@ -0,0 +1,13 @@
+import AppGuard from '@/components/AppGuard';
+
+export default function AppLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <AppGuard>
+      {children}
+    </AppGuard>
+  );
+}
