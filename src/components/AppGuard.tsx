563da8065e38279b31cb26c992edc1c8e78d50e0
ibrahimh-max <ih8445807@gmail.com>
2026-06-16 00:37:46 +0530

    confirm email enabled

--- src/components/AppGuard.tsx
+++ src/components/AppGuard.tsx
@@ -1,27 +1,9 @@
 'use client';
 
-import { useEffect } from 'react';
-import { useRouter } from 'next/navigation';
-import { Capacitor } from '@capacitor/core';
-
 export default function AppGuard({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  const router = useRouter();
-
-  const isNative = Capacitor.isNativePlatform();
-
-  useEffect(() => {
-    if (!isNative) {
-      router.replace('/');
-    }
-  }, [router, isNative]);
-
-  if (!isNative) {
-    return null;
-  }
-
   return <>{children}</>;
 }
